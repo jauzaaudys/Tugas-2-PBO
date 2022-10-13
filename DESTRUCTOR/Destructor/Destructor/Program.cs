4fb3c4f67ea20297b47b3ba456d04259c3aacf9b
jauzaaudys <115676657+jauzaaudys@users.noreply.github.com>
2022-10-13 23:40:11 +0700

    Destructor
    Destructor pada C# beserta tampilan ketika program dijalankan

---
+++ DESTRUCTOR/Destructor/Destructor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Destructor
+{
+    class Murid
+    {
+        public Murid()
+        {
+            Console.WriteLine("Contoh Program Destructor");
+            Console.WriteLine("=========================");
+        }
+        // Destructor
+        ~Murid()
+        {
+            Console.WriteLine("Jauza Audy Safitri");
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Jalankan();
+            GC.Collect();
+
+        }
+        public static void Jalankan()
+        {
+            // Membuat Instance Class
+            Murid murid = new Murid();
+        }
+    }
+}
