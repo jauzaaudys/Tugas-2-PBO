24f217f001f32358f87633fad72026e33de1df8c
jauzaaudys <115676657+jauzaaudys@users.noreply.github.com>
2022-10-13 23:44:43 +0700

    Constructor
    Constructor pada C# beserta tampilan ketika program dijalankan

---
+++ CONSTRUCTOR/Constructor/Constructor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Constructor
+{
+    public class Constructor_Static
+    {
+        //static data member   
+        private static int nim = 071;
+        public static int Id
+        {
+            get
+            {
+                return nim;
+            }
+        }
+        public static void print()
+        {
+            Console.WriteLine("Contoh Constructor");
+            Console.WriteLine("==================");
+            Console.WriteLine("Jauza Audy Safitri = " + nim);
+        }
+        public static void Main(string[] args)
+        {
+            //Print Value nim 
+            Constructor_Static.print();
+        }
+    }
+}
